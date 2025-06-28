c7793cc156ced87b920ee38f57b3c94b7c772039
Alegnta <90365272+alegnta88@users.noreply.github.com>
2025-06-28 22:00:33 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,8 @@ window._config = {
     cognito: {
         userPoolId: 'eu-north-1_MPSJQ0LEL', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '1l9ladaee0172bst2jo392qfmg', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'eu-north-1' // e.g. us-east-2
+        region: 'eu-north-1',
+        SecretHash: 'AmWAwXOZWfg1nBn6PoEIfkeSE7CmoOgxnwxljmg1g/Y=' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
